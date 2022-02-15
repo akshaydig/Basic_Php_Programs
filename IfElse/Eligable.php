1528822b90fbdaf03c9feac7f3115a2ed68da145
akshaydig <you@example.com>
2022-02-15 11:28:58 +0530

    Added : Basic Php Program using IfElse

---
+++ IfElse/Eligable.php
@@ -0,0 +1,11 @@
+<?php  
+ $age = 23;  
+$nationality = "Indian";  
+if ($nationality == "Indian"){
+    if ($age >= 18) {  
+        echo "Eligible to give vote";  
+    }else {    
+        echo "Not eligible to give vote";  
+    }  
+}  
+?>  
---
+++ IfElse/LeapYear.php
@@ -0,0 +1,9 @@
+<?php
+$a = readline('Enter the Number : ');
+
+if ($a % 4 == 0){
+    echo "Number is Leap Year"; 
+} else {
+    echo "Number is Not Leap Year"; 
+}
+?>
