5396c5b7f45add18a6ff080b3f561653ea3b2ce7
akshaydig <you@example.com>
2022-02-15 11:30:44 +0530

    Added : Basic Php Program using WhileLoop

---
+++ WhileLoop/EvenOdd.php
@@ -0,0 +1,12 @@
+<?php
+$a = 1;
+    while ($a <= 100) {
+        if ($a % 2 == 0) {
+            echo "\nEven Number :".$a;
+        } else {
+            echo "\nOdd Number :".$a;
+        }
+        $a++;
+    }
+
+?>
